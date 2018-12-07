2f150bd2bdbab1757d58552e90a5a7973792ddf3
Yanimoghraoui <yani.mog@gmail.com>
2018-12-07 19:38:21 +0100

    connect sucess

--- client.c
+++ client.c
@@ -9,8 +9,7 @@
 #include <sys/socket.h>
 #define BUFFER_SIZE 256
 
-//coucou !!!
-
+	//socket client
 int cree_socket_tcp_client()
 {
   struct sockaddr_in serveur_adresse;
@@ -36,49 +35,70 @@ int cree_socket_tcp_client()
 
   nb= read(socket_client, bufferR, BUFFER_SIZE);
   printf("%s \n",bufferR);
-
-  
-  
-  
+ 
   return socket_client;
 }
 
-int connexion(int socket_contact) {
-    struct sockaddr_in adresse;
-    int socket_connectee;
-    socklen_t longueur;
-    socket_connectee = accept(socket_contact,
-    (struct sockaddr*)&adresse,&longueur);
-    if (socket_connectee < 0){
-        perror( "accept" );
-        close(socket_contact);
-        exit(11);
-    }
-    return socket_connectee;
-}
 
-
-int affiche_adresse_socket(int sock){
-    struct sockaddr_in adresse;
-    socklen_t longueur;
-    longueur = sizeof(struct sockaddr_in);
-    if (getsockname(sock, (struct sockaddr*)&adresse, &longueur) < 0){
-        close(sock);
-        perror("Erreur getsockname");
-        exit(3);
-    }
-    printf("IP = %s, Port = %u\n", inet_ntoa(adresse.sin_addr), ntohs(adresse.sin_port));
-    return 0;
-}
-
-
-int main()
+	//Affiche
+int affiche_adresse_socket(int sock)
 {
-  if(cree_socket_tcp_client() == -1) 
+  struct sockaddr_in adresse;
+  socklen_t longueur;
+  longueur = sizeof(struct sockaddr_in);
+  if (getsockname(sock, (struct sockaddr*)&adresse, &longueur) < 0)
   {
-	fprintf(stderr, "Erreur création client \n");
-	return -1; 
+     fprintf(stderr, "Erreur getsockname");
+     return -1;
   }
+  printf("IP = %s, Port = %u\n", inet_ntoa(adresse.sin_addr), ntohs(adresse.sin_port));
+  return 0;
+}
+
+	
+
+int affiche_adresse_distant(int sock)
+{
+  struct sockaddr_in adresse;
+  socklen_t longueur;
+  longueur = sizeof(struct sockaddr_in);
+  if (getpeername(sock, (struct sockaddr*)&adresse, &longueur) < 0)
+  {
+     fprintf(stderr, "Erreur getpeername");
+     return -1;
+  }
+  printf("IP = %s, Port = %u\n", inet_ntoa(adresse.sin_addr), ntohs(adresse.sin_port));
+  return 0;
+}
+
+void communication( int sock){
+  struct sockaddr_in adress;
+  socklen_t lg;
+  char bufferR[BUFFER_SIZE];
+  char bufferW[BUFFER_SIZE];
+  int nb;
+  lg = sizeof(struct sockaddr_in);
+  if (getpeername(sock, (struct sockaddr*) &adress, &lg) < 0) {
+    fprintf(stderr, "Erreur getpeername");
+    return;
+  }
+  printf("Connexion : locale (sock_connectee)");
+  affiche_adresse_socket(sock);
+  strcpy(bufferW, "Veuillez entrer une phrase : ");
+  write(sock, bufferW, strlen(bufferW)+1);
+  nb= read(sock, bufferR, BUFFER_SIZE);
+  bufferR[nb-2] = '\0';
+  printf("L’utilisateur distant a tapé: %s", bufferR);
+  sprintf(bufferW, "Merci!");
+  write(sock, bufferR, BUFFER_SIZE);
 
 }
-  %contour associe chaque point à une valeur 
+
+int main() {
+    printf("Bonjour Je suis le client!\n");
+    int socket_client;// creation socket locale client pour la connection machine distante: 
+    socket_client = cree_socket_tcp_client(); 
+    affiche_adresse_socket(socket_client);// affichage adresse socket serveur locale:	
+    printf("Au revoir!\n");        // termine:
+}
+
--- serveur.c
+++ serveur.c
@@ -32,22 +32,52 @@ int cree_socket_tcp_ip_serveur()
   return socket_serveur;
 }
 
-
-int affiche_serveur_adresse_socket(int sock)
+int affiche_adresse_socket(int sock)
 {
-  struct sockaddr_in serveur_adresse;
+  struct sockaddr_in adresse;
   socklen_t longueur;
   longueur = sizeof(struct sockaddr_in);
-  if (getsockname(sock, (struct sockaddr*)&serveur_adresse, &longueur) < 0)
+  if (getsockname(sock, (struct sockaddr*)&adresse, &longueur) < 0)
   {
      fprintf(stderr, "Erreur getsockname");
      return -1;
   }
-  printf("IP = %s, Port = %u\n", 
-inet_ntoa(serveur_adresse.sin_addr), 
-ntohs(serveur_adresse.sin_port));
+  printf("IP = %s, Port = %u\n", inet_ntoa(adresse.sin_addr), ntohs(adresse.sin_port));
   return 0;
 }
+
+
+int connexion (int socket_contact){
+    int sock_connectee;
+    struct sockaddr_in adress;
+    socklen_t lg;
+    listen(socket_contact, 1);
+    lg = sizeof(struct sockaddr_in);
+    sock_connectee = accept(socket_contact, (struct sockaddr*)&adress, &lg);
+    if (sock_connectee < 0){
+        fprintf(stderr, "erreur accept ");
+        return -1;
+    }
+
+    close(socket_contact);
+    return sock_connectee;
+}
+
+
+int affiche_adresse_distant(int sock)
+{
+  struct sockaddr_in adresse;
+  socklen_t longueur;
+  longueur = sizeof(struct sockaddr_in);
+  if (getpeername(sock, (struct sockaddr*)&adresse, &longueur) < 0)
+  {
+     fprintf(stderr, "Erreur getpeername");
+     return -1;
+  }
+  printf("IP = %s, Port = %u\n", inet_ntoa(adresse.sin_addr), ntohs(adresse.sin_port));
+  return 0;
+}
+
 
 
 void traite_connection(int sock)
@@ -63,7 +93,7 @@ void traite_connection(int sock)
     return;
   }
   printf("Connexion : locale (sock_connectee)");    
-  affiche_serveur_adresse_socket(sock);
+  affiche_adresse_distant(sock);
   strcpy(bufferW, "Veuillez entrer une phrase : ");
   write(sock, bufferW, strlen(bufferW)+1);
   nb= read(sock, bufferR, BUFFER_SIZE);
@@ -73,9 +103,57 @@ void traite_connection(int sock)
   write(sock, bufferR, BUFFER_SIZE);
 }
 
+/*
+void traitement( int socket_connectee ) {
+char buffer[BUFFER_SIZE];
+int nb;
+// envoi du message d'accueil:
+strcpy( buffer, "Bonjour, je suis la machine locale." );
+write( socket_connectee, buffer, strlen(buffer)+1 );
+// reception de la reponse de la machine distante:
+nb= read(socket_connectee, buffer, BUFFER_SIZE);
+buffer[nb-2] = '\0';
+// affichage:
+printf( "Texte recu de la machine distante: %s\n", buffer );
+}
+*/
+
+void communication( int sock){
+  struct sockaddr_in adress;
+  socklen_t lg;
+  char bufferR[BUFFER_SIZE];
+  char bufferW[BUFFER_SIZE];
+  int nb;
+  lg = sizeof(struct sockaddr_in);
+  if (getpeername(sock, (struct sockaddr*) &adress, &lg) < 0) {
+    fprintf(stderr, "Erreur getpeername");
+    return;
+  }
+  printf("Connexion : locale (sock_connectee)");
+  affiche_adresse_socket(sock);
+  strcpy(bufferW, "Veuillez entrer une phrase : ");
+  write(sock, bufferW, strlen(bufferW)+1);
+  nb= read(sock, bufferR, BUFFER_SIZE);
+  bufferR[nb-2] = '\0';
+  printf("L’utilisateur distant a tapé: %s", bufferR);
+  sprintf(bufferW, "Merci!");
+  write(sock, bufferR, BUFFER_SIZE);
+
+}
+
+
+/*
 int main(void)
 {
+	printf("Bonjour Je suis le serveur!\n");
+    int socket_serveur;// creation socket de serveur pour les connections: 
+    int socket_client;  //pour relier la machine distante à une socket "client"
+
+    printf("Au revoir!\n");        // termine:
+	
   int sock_contact; int sock_connectee;
+  int socket_serveur;// creation socket de serveur pour les connections: 
+  int socket_client;  //pour relier la machine distante à une socket "client"
   struct sockaddr_in serveur_adresse;
   socklen_t lg;
   pid_t pid_fils;
@@ -93,13 +171,32 @@ int main(void)
         fprintf(stderr, "Erreur fork\n");
         return -1;
       }
-      if (pid_fils == 0)  {/* fils */
+      if (pid_fils == 0)  {// fils 
         close(sock_contact);
         traite_connection(sock_connectee);
         exit(0);
-      } else              /* pere */
+      } else              // pere 
         close(sock_connectee);
     }
     
 return 0; 
+}
+*/
+
+
+
+//Code main du serveur de base sans boucle while
+
+int main() {
+    printf("Bonjour Je suis le client!\n");
+    int socket_serveur;// creation socket de serveur pour les connections: 
+    int socket_client;  //pour relier la machine distante à une socket "client"
+    socket_serveur = cree_socket_tcp_ip_serveur(); 
+    affiche_adresse_socket(socket_serveur);// affichage adresse socket serveur locale:
+    socket_client = connexion( socket_serveur ); // accepte co sur serveur et liaison à socket "client"
+    traite_connection(socket_client);
+	close(socket_serveur);      // on continue avec la socket connectee:  
+    affiche_adresse_distant(socket_client);		
+    communication(socket_client); 
+    printf("Au revoir!\n");        // termine:
 }
